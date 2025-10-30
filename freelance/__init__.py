42d135baff4e920057227d0617e95ccbc8e3b6f1
66070271 <141338278+66070271@users.noreply.github.com>
2025-10-31 03:33:16 +0700

    Update __init__.py

--- freelance/__init__.py
+++ freelance/__init__.py
@@ -1,5 +1,11 @@
-from django.contrib.staticfiles import storage
-from freelance.storages_backends import StaticStorage
+from django.contrib.staticfiles import storage as static_storage
+from django.core.files import storage as file_storage
+from freelance.storages_backends import StaticStorage, MediaStorage
 
-storage.staticfiles_storage = StaticStorage()
-print("⚙️ STATICFILES_STORAGE now uses:", storage.staticfiles_storage.__class__)
+# --- Force Django to use S3 for static files ---
+static_storage.staticfiles_storage = StaticStorage()
+print("⚙️ STATICFILES_STORAGE now uses:", static_storage.staticfiles_storage.__class__)
+
+# --- Force Django to use S3 for media uploads ---
+file_storage.default_storage = MediaStorage()
+print("📦 DEFAULT_FILE_STORAGE now uses:", file_storage.default_storage.__class__)
